c143169fb9b6d62b01c601f41bef91668235facb
Vinh Thanh Vu <vtv@vinh.com>
2015-09-26 18:55:50 -0400

    For-in: Example 2

--- Lesson4_ControlFlow.playground/Contents.swift
+++ Lesson4_ControlFlow.playground/Contents.swift
@@ -10,7 +10,7 @@ for var index = 99; index > 0; --index {
 }
 
 // II. Fast enumeration with for-in
-// For-iin loops take the form:
+// For-in loops take the form:
 
 // for item in Collection {
 //        statements to execute on each item
@@ -22,3 +22,12 @@ var demoString = "Swift enumeration is so fast!"
 for character in demoString {
     print(character)
 }
+
+// Example 2
+let intArray = [7, 21, 25, 13, 1]
+var sum = 0
+for value in intArray {
+    sum += value
+}
+sum
+
